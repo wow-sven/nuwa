04d71bc4bad6969c94d6c9f1e7082746de12cef8
Mine77 <haichaozhu77@gmail.com>
2025-04-25 00:04:13 +0800

    [Campaign] increase maxSteps

--- website/campaign/src/app/api/bot/ai-utils.ts
+++ website/campaign/src/app/api/bot/ai-utils.ts
@@ -21,7 +21,7 @@ export async function generateAndSendAIResponse(
         messages: history,
         tools,
         system: systemPrompt,
-        maxSteps: 5,
+        maxSteps: 10,
         toolChoice: 'auto'
     });
 
--- website/campaign/src/app/api/chat/route.ts
+++ website/campaign/src/app/api/chat/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
         messages,
         tools,
         system: systemPrompt,
-        maxSteps: 5,
+        maxSteps: 10,
         toolChoice: 'auto',
         onError() { }
     });
